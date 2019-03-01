8489e881da02e7cde47dc9f2b07debb485d10b2f
Sang Dang <sangdth@gmail.com>
2019-03-01 21:35:43 +0200

    single page work :D

--- pages/paths/_slug.vue
+++ pages/paths/_slug.vue
@@ -1,19 +1,44 @@
 <template>
-    <div class="wrapper">
-        <h1>{{ name }} <small>{{ description }}</small></h1>
-        <el-button type="success">
-            Enroll Now
-        </el-button>
-        <el-collapse v-model="activeItems" @change="handleChange">
-            <el-collapse-item
-                v-for="sprint in sprints"
-                :key="sprint._id"
-                :title="sprint.name"
-                :name="sprint._id"
-            >
-                <div>{{ sprint.description }}</div>
-            </el-collapse-item>
-        </el-collapse>
+    <div class="single-path">
+        <div class="path-content">
+            <div class="block">
+                <h1>{{ name }} <small>{{ description }}</small></h1>
+                <el-button type="success">
+                    Enroll Now
+                </el-button>
+            </div>
+            <div class="sprints block">
+                <el-row :gutter="10">
+                    <el-col :span="16">
+                        <h3>Sprints</h3>
+                    </el-col>
+                    <el-col :span="8">
+                        <h5>Materials: 1024</h5>
+                    </el-col>
+                </el-row>
+
+                <el-collapse v-model="activeItems" @change="handleChange">
+                    <el-collapse-item
+                        v-for="sprint in sprints"
+                        :key="sprint._id"
+                        :title="sprint.name"
+                        :name="sprint._id"
+                    >
+                        <el-row :gutter="10">
+                            <el-col :span="16">
+                                <p>{{ sprint.description }}</p>
+                            </el-col>
+                            <el-col :span="8">
+                                <p>241</p>
+                            </el-col>
+                        </el-row>
+                    </el-collapse-item>
+                </el-collapse>
+            </div>
+        </div>
+        <div class="path-image is-extra-large">
+            <img :src="image" class="image">
+        </div>
     </div>
 </template>
 
@@ -51,4 +76,122 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.single-path {
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    margin-bottom: 50px;
+
+    .project-content {
+        padding: 20px !important;
+        height: auto;
+
+        .project-head {
+
+            color: #FFF;
+
+            .desc {
+                color: #FFF;
+                font-size: 90%;
+                margin-bottom: 10px;
+            }
+        }
+
+        .block {
+            display: block;
+            width: 100%;
+            clear: both;
+        }
+
+        .wide-title {
+            float: left;
+            display: block;
+            width: 100%;
+            clear: both;
+            color: #EEE;
+            border-bottom: 1px solid #CCCCCC50;
+        }
+
+        .short-title {
+            display: inline-block;
+            line-height: 38px;
+            margin: 0 10px 0 0;
+
+            &:hover {
+                cursor: pointer;
+            }
+        }
+
+        .sort-icon {
+            &:hover {
+                cursor: pointer;
+            }
+        }
+    }
+
+    .right {
+        float:right;
+    }
+
+    .report-btn {
+        margin-left: 10px;
+        width: 110px;
+    }
+
+    .download-link {
+        font-size: 90%;
+        font-weight: bold;
+        color: #FFF;
+    }
+    .download-button {
+        width: 110px;
+    }
+
+    .file-name {
+        margin: 0;
+        font-weight: bold;
+    }
+    .file-description {
+        font-size: 90%;
+        margin: 0;
+        line-height: 14px;
+    }
+
+    .switch-text {
+        font-size: 80% !important;
+        margin-left: 5px;
+    }
+
+    .attachments-buttons {
+        display: flex;
+        justify-content: flex-end;
+
+        .delete-btn {
+            margin-left: 10px;
+        }
+    }
+
+    .el-table {
+        margin-bottom: 35px;
+        clear: both;
+
+    }
+
+    h1 {
+        font-size: 60px;
+    }
+    h1 small {
+        display: block;
+        color: #999;
+        font-size: 55%;
+        font-weight: 200 !important;
+    }
+    .image {
+        width: 100%;
+    }
+    .block {
+        width: 100%;
+    }
+
+}
 </style>
